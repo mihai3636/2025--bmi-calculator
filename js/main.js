17fd439620828e5df1ecbac1e3a4fc106969fd83
Mihai Dogaru <dogarumihai2537@gmail.com>
2026-02-09 20:56:34 +0200

    Add JS for radio switcher

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 console.log("Hello world!");
 
 const wrappers = document.querySelectorAll(".input-wrapper");
+const inputMetricEl = document.getElementById("radioMetric");
+const inputImperialEl = document.getElementById("radioImperial");
+const resultEl = document.querySelector(".result");
+const calculatorEl = document.querySelector(".calculator");
+
+const heightMainUnitEl = document.getElementById("heightMainUnit");
+const heightSecondaryUnitEl = document.getElementById("heightSecondaryUnit");
+
+const weightMainUnitEl = document.getElementById("weightMainUnit");
+const weightSecondaryUnitEl = document.getElementById("weightSecondaryUnit");
+
+console.log(
+  heightMainUnitEl,
+  heightSecondaryUnitEl,
+  weightMainUnitEl,
+  weightSecondaryUnitEl,
+);
+
+let isMetric = false;
+let isImperial = true;
 
 wrappers.forEach((wrapperEl) => {
   wrapperEl.addEventListener("click", (e) => {
@@ -12,3 +32,42 @@ wrappers.forEach((wrapperEl) => {
     inputEl.setSelectionRange(length, length);
   });
 });
+
+inputMetricEl.addEventListener("click", onMetricClicked);
+inputImperialEl.addEventListener("click", onImperialClicked);
+updateUi();
+
+function updateUi() {
+  if (isMetric) showMetric();
+  if (isImperial) showImperial();
+}
+
+function onMetricClicked() {
+  isMetric = true;
+  isImperial = false;
+
+  updateUi();
+  console.log(`Metric clicked`);
+}
+
+function onImperialClicked() {
+  isImperial = true;
+  isMetric = false;
+
+  updateUi();
+  console.log(`Imperial clicked`);
+}
+
+function showMetric() {
+  calculatorEl.classList.add("metric");
+  inputMetricEl.checked = true;
+  heightMainUnitEl.textContent = "m";
+  weightMainUnitEl.textContent = "kg";
+}
+
+function showImperial() {
+  calculatorEl.classList.remove("metric");
+  inputImperialEl.checked = true;
+  heightMainUnitEl.textContent = "ft";
+  weightMainUnitEl.textContent = "st";
+}
